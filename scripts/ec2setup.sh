9b25671231a43539f9e3566574e361f57bbd8b92
Krishna Kant Sharma <kks@krishnakantsharma.com>
2016-04-07 10:54:35 +0530

    fixes

--- scripts/ec2setup.sh
+++ scripts/ec2setup.sh
@@ -4,24 +4,24 @@
 # no current parameters to the script
 
 # run update
-yum update
+sudo yum update
 
 # enable epel
-yum-config-manager --enable epel && yum clean all 
+sudo yum-config-manager --enable epel && yum clean all 
 
 # install node
-yum install nodejs npm
+sudo yum install nodejs npm
 
 
 # adjust file limits etc
-cat <<EOT >> /etc/security/limits.d/custom.conf
+sudo cat <<EOT >> /etc/security/limits.d/custom.conf
 root soft nofile 1000000
 root hard nofile 1000000
 * soft nofile 1000000
 * hard nofile 1000000
 EOT
 
-cat <<EOT >> /etc/sysctl.conf
+sudo cat <<EOT >> /etc/sysctl.conf
 fs.file-max = 1000000
 fs.nr_open = 1000000       
 net.ipv4.netfilter.ip_conntrack_max = 1048576
@@ -32,15 +32,16 @@ sudo sh -c "ulimit -n 100000 && exec su $LOGNAME"
 
 
 # install git
-yum install git
+sudo yum install git
 
 # setup tester app
-mkdir /usr/apps
+sudo mkdir /usr/apps
+sudo chown -R ec2-user:ec2-user /usr/apps
 cd /usr/apps
 # lets clone full repo. might wanna fetch changes too
 git clone https://github.com/kksharma1618/websocket-bench kkwebsocketbench
 # link for command access
 cd /usr/apps/kkwebsocketbench
-npm link
+sudo npm link
 
 
