b39d5135f0948e38fb1b8133d93873061c407a62
Krishna Kant Sharma <kks@krishnakantsharma.com>
2016-04-07 13:33:27 +0530

    fixes

--- scripts/ec2setup.sh
+++ scripts/ec2setup.sh
@@ -4,35 +4,38 @@
 # no current parameters to the script
 
 # run update
-sudo yum update
+sudo yum update -y
 
 # enable epel
 sudo yum-config-manager --enable epel
 
 # install node
-sudo yum install nodejs npm
+sudo yum install -y nodejs npm
 
 
 # adjust file limits etc
-sudo cat <<EOT >> /etc/security/limits.d/custom.conf
+sudo tee -a /etc/security/limits.d/custom.conf >/dev/null <<EOT
 root soft nofile 1000000
 root hard nofile 1000000
 * soft nofile 1000000
 * hard nofile 1000000
+
 EOT
 
-sudo cat <<EOT >> /etc/sysctl.conf
+
+sudo tee -a /etc/sysctl.conf >/dev/null <<EOT
 fs.file-max = 1000000
 fs.nr_open = 1000000       
 net.ipv4.netfilter.ip_conntrack_max = 1048576
 net.nf_conntrack_max = 1048576
+
 EOT
 
 sudo sh -c "ulimit -n 100000 && exec su $LOGNAME"
 
 
 # install git
-sudo yum install git
+sudo yum install -y git
 
 # setup tester app
 sudo mkdir /usr/apps
